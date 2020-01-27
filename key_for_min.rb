5050894e09d3f003ec0535ca4c769d4062d7cb83
Diego Vasquez <botellongo69@outlook.com>
2020-01-27 22:45:41 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 def key_for_min_value(name_hash)
   smallestKey = ''
+<<<<<<< HEAD
   smallestValue = 9999
   if name_hash.empty?
     nil 
@@ -14,4 +15,19 @@ def key_for_min_value(name_hash)
     end
     smallestKey
   end
+=======
+  if name_hash.empty?
+    return nil
+  else
+    name_hash.each do |key, value|
+      smallestKey = key
+      smallestValue = value
+
+      if smallestValue > value
+        smallestKey =  key
+      end
+    end
+  end
+  smallestKey
+>>>>>>> 9ecea3546129494467c73a07813fd5bd4eb6a5a3
 end
